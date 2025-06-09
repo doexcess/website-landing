6cf018c8be06f80162d4d871d93ea055a3c18b93
Ihunna Promise <pihunna@yahoo.com>
2025-06-09 01:58:04 +0100

    feat: font update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,33 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
+import localFont from "next/font/local";
+
+const Gilroy = localFont({
+  src: [
+    {
+      path: "./fonts/gilroy/Gilroy-Light.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-Medium.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-Bold.ttf",
+      weight: "800",
+      style: "bold",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "DoExcess",
@@ -11,15 +37,15 @@ export const metadata: Metadata = {
   },
 };
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export default function RootLayout({
   children,
@@ -29,7 +55,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${Gilroy.className} antialiased`}
         style={{ backgroundColor: "#fff" }}
       >
         <Provider>{children}</Provider>
